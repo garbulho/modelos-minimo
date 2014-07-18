824ca5525285ccb4dc5a63ffc83dd42d36c6ede7
Tuanny Harumi <tuanny.harumi@gmail.com>
2014-07-18 09:18:11 -0300

    oremos

--- js/elements.js
+++ js/elements.js
@@ -8,250 +8,310 @@
 // 7: acessorios
 
 globalElements = {
-	roupas: [
+	"roupas": [
 		{
-			title: "Calcinha 00",
-			id: "calcinha_00",
-			type: "panties",
-			position: {
-				top: 233,
-				left: 179 
+			"title": "Calcinha 00",
+			"id": "calcinha_00",
+			"type": "panties",
+			"position": {
+				"top": 233,
+				"left": 179 
 			},
-			layer: 3,
-			marca: ""
+			"layer": 3,
+			"marca": "",
+			"isDressed": true,
+			"fav": false
 		},
 		{
-			title: "Soutien 00",
-			id: "soutien_00",
-			type: "soutien",
-			position: {
-				top: 132,
-				left: 188 
+			"title": "Soutien 00",
+			"id": "soutien_00",
+			"type": "soutien",
+			"position": {
+				"top": 132,
+				"left": 188 
 			},
-			layer: 3,
-			marca: ""
+			"layer": 3,
+			"marca": "",
+			"isDressed": true,
+			"fav": false
 		},
 		{
-			title: "Calça 00",
-			id: "calca_00",
-			type: "bottom",
-			position: {
-				top: 235,
-				left: 177 
+			"title": "Calça 00",
+			"id": "calca_00",
+			"type": "bottom",
+			"position": {
+				"top": 235,
+				"left": 177 
 			},
-			layer: 5,
-			marca: ""
+			"layer": 5,
+			"marca": "",
+			"isDressed": false,
+			"fav": false
 		},
 		{
-			title: "Saia 00",
-			id: "saia_00",
-			type: "bottom",
-			position: {
-				top: 224,
-				left: 166 
+			"title": "Saia 00",
+			"id": "saia_00",
+			"type": "bottom",
+			"position": {
+				"top": 224,
+				"left": 166 
 			},
-			layer: 5,
-			marca: ""
+			"layer": 5,
+			"marca": "",
+			"isDressed": false,
+			"fav": false
 		},
 		{
-			title: "Saia 01",
-			id: "saia_01",
-			type: "bottom",
-			position: {
-				top: 224,
-				left: 166 
+			"title": "Saia 01",
+			"id": "saia_01",
+			"type": "bottom",
+			"position": {
+				"top": 224,
+				"left": 166 
 			},
-			layer: 5,
-			marca: ""
+			"layer": 5,
+			"marca": "",
+			"isDressed": false,
+			"fav": false
 		},
 		{
-			title: "Saia 02",
-			id: "saia_02",
-			type: "bottom",
-			position: {
-				top: 224,
-				left: 166 
+			"title": "Saia 02",
+			"id": "saia_02",
+			"type": "bottom",
+			"position": {
+				"top": 224,
+				"left": 166 
 			},
-			layer: 5,
-			marca: ""
+			"layer": 5,
+			"marca": "",
+			"isDressed": false,
+			"fav": false
 		},
 		{
-			title: "Camiseta 00",
-			id: "camiseta_00",
-			type: "top",
-			position: {
-				top: 127,
-				left: 158 
+			"title": "Camiseta 00",
+			"id": "camiseta_00",
+			"type": "top",
+			"position": {
+				"top": 127,
+				"left": 158 
 			},
-			layer: 6,
-			marca: ""
+			"layer": 6,
+			"marca": "",
+			"isDressed": false,
+			"fav": false
 		},
 		{
-			title: "Camiseta 01",
-			id: "camiseta_01",
-			type: "top",
-			position: {
-				top: 127,
-				left: 158 
+			"title": "Camiseta 01",
+			"id": "camiseta_01",
+			"type": "top",
+			"position": {
+				"top": 127,
+				"left": 158 
 			},
-			layer: 6,
-			marca: ""
+			"layer": 6,
+			"marca": "",
+			"isDressed": false,
+			"fav": false
 		},
 		{
-			title: "Camiseta 02",
-			id: "camiseta_02",
-			type: "top",
-			position: {
-				top: 127,
-				left: 158 
+			"title": "Camiseta 02",
+			"id": "camiseta_02",
+			"type": "top",
+			"position": {
+				"top": 127,
+				"left": 158 
 			},
-			layer: 6,
-			marca: ""
+			"layer": 6,
+			"marca": "",
+			"isDressed": false,
+			"fav": false
 		},
 		{
-			title: "Camiseta 03",
-			id: "camiseta_03",
-			type: "top",
-			position: {
-				top: 127,
-				left: 158 
+			"title": "Camiseta 03",
+			"id": "camiseta_03",
+			"type": "top",
+			"position": {
+				"top": 127,
+				"left": 158 
 			},
-			layer: 6,
-			marca: ""
+			"layer": 6,
+			"marca": "",
+			"isDressed": false,
+			"fav": false
 		},
 		{
-			title: "Camiseta 04",
-			id: "camiseta_04",
-			type: "top",
-			position: {
-				top: 127,
-				left: 158 
+			"title": "Camiseta 04",
+			"id": "camiseta_04",
+			"type": "top",
+			"position": {
+				"top": 127,
+				"left": 158 
 			},
-			layer: 6,
-			marca: ""
+			"layer": 6,
+			"marca": "",
+			"isDressed": false,
+			"fav": false
 		},
 		{
-			title: "Camiseta 05",
-			id: "camiseta_05",
-			type: "top",
-			position: {
-				top: 127,
-				left: 158 
+			"title": "Camiseta 05",
+			"id": "camiseta_05",
+			"type": "top",
+			"position": {
+				"top": 127,
+				"left": 158 
 			},
-			layer: 6,
-			marca: ""
+			"layer": 6,
+			"marca": "",
+			"isDressed": false,
+			"fav": false
 		},
 		{
-			title: "Colar 00",
-			id: "colar_00",
-			type: "accessories",
-			position: {
-				top: 122,
-				left: 204 
+			"title": "Colar 00",
+			"id": "colar_00",
+			"type": "accessories",
+			"position": {
+				"top": 122,
+				"left": 204 
 			},
-			layer: 7,
-			marca: ""
+			"layer": 7,
+			"marca": "",
+			"isDressed": false,
+			"fav": false
 		},
 		{
-			title: "Colar 01",
-			id: "colar_01",
-			type: "accessories",
-			position: {
-				top: 122,
-				left: 204 
+			"title": "Colar 01",
+			"id": "colar_01",
+			"type": "accessories",
+			"position": {
+				"top": 122,
+				"left": 204 
 			},
-			layer: 7,
-			marca: ""
+			"layer": 7,
+			"marca": "",
+			"isDressed": false,
+			"fav": false
 		},
 		{
-			title: "Maiô 00",
-			id: "maio_00",
-			type: "swimsuit",
-			position: {
-				top: 130,
-				left: 171 
+			"title": "Maiô 00",
+			"id": "maio_00",
+			"type": "swimsuit",
+			"position": {
+				"top": 130,
+				"left": 171 
 			},
-			layer: 3,
-			marca: ""
+			"layer": 3,
+			"marca": "",
+			"isDressed": false,
+			"fav": false
 		},
 		{
-			title: "Maiô 01",
-			id: "maio_01",
-			type: "swimsuit",
-			position: {
-				top: 130,
-				left: 171 
+			"title": "Maiô 01",
+			"id": "maio_01",
+			"type": "swimsuit",
+			"position": {
+				"top": 130,
+				"left": 171 
 			},
-			layer: 3,
-			marca: ""
+			"layer": 3,
+			"marca": "",
+			"isDressed": false,
+			"fav": false
 		},
 		{
-			title: "Sapato 00",
-			id: "sapato_00",
-			type: "shoes",
-			position: {
-				top: 471,
-				left: 198 
+			"title": "Sapato 00",
+			"id": "sapato_00",
+			"type": "shoes",
+			"position": {
+				"top": 471,
+				"left": 198 
 			},
-			layer: 4,
-			marca: ""
+			"layer": 4,
+			"marca": "",
+			"isDressed": false,
+			"fav": false
 		}
 	],
-	modelo: [
+	"modelo": [
 		{
-			id: "modelo_00",
-			type: "modelo",
-			position: {
-				top: 0,
-				left: 0 
-			}
+			"id": "modelo_00",
+			"type": "modelo",
+			"position": {
+				"top": 0,
+				"left": 0 
+			},
+			"isDressed": true,
+			"fav": false
 		},
 		{
-			id: "modelo_01",
-			type: "modelo",
-			position: {
-				top: 0,
-				left: 0 
-			}
+			"id": "modelo_01",
+			"type": "modelo",
+			"position": {
+				"top": 0,
+				"left": 0 
+			},
+			"isDressed": false,
+			"fav": false
 		}
 	],
-	fundo: [ 
+	"fundo": [ 
 		{
-			id: "background_00",
-			type: "background",
-			title: "Background A"
+			"id": "background_00",
+			"type": "background",
+			"title": "Background A",
+			"isDressed": true,
+			"fav": false
 		},
 		{
-			id: "background_01",
-			type: "background",
-			title: "Background B"
+			"id": "background_01",
+			"type": "background",
+			"title": "Background B",
+			"isDressed": false,
+			"fav": false
 		},
 		{
-			id: "background_02",
-			type: "background",
-			title: "Background C"
+			"id": "background_02",
+			"type": "background",
+			"title": "Background C",
+			"isDressed": false,
+			"fav": false
 		}
 	],
-	efeitos: [
+	"efeitos": [
 		{
-			id: "sombra_00",
-			title: "Sombra 01",
-			type: "shadow",
-			position: {
-				top: 315,
-				left: 171 
+			"id": "sombra_00",
+			"title": "Sombra 01",
+			"type": "shadow",
+			"position": {
+				"top": 315,
+				"left": 171 
 			},
-			layer: 1
+			"layer": 1,
+			"isDressed": false,
+			"fav": false
 		},
 		{
-			id: "sombra_01",
-			title: "Sombra 02",
-			type: "shadow",
-			position: {
-				top: 485,
-				left: 105 
+			"id": "sombra_01",
+			"title": "Sombra 02",
+			"type": "shadow",
+			"position": {
+				"top": 485,
+				"left": 105 
 			},
-			layer: 1
+			"layer": 1
 		}
 	]
-}
+};
+
+recent = [
+			{"id": "calcinha_00"},
+			{"id": "soutien_00"},
+			{"id": "calca_00"},
+			{"id": "saia_00"},
+	     ];
+recomended = [
+			{"id": "colar_00"},
+			{"id": "colar_01"},
+			{"id": "maio_00"},
+			{"id": "maio_01"},
+			{"id": "sapato_00"},
+			]
--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,88 @@
+//globalElements;
 draggedElem = null;
+
+function createObject() {
+	/*$.getJSON("COLOCAR AQUI O ENDEREÇO", function(data) {
+	    console.log(data);
+	    //globalElements = data;
+	    //globalElements = JSON.parse(data);
+	});*/
+}
+
+function getContent(type) {
+	/*var url;
+	if (type === "dressed") {
+		url = "";
+	} else if (type === "fav") {
+		url = "";
+	} else if (type === "recomended") {
+		url = "";
+	} else if (type === "recent") {
+		url = "";
+	}
+	$.getJSON(url, function(data) {
+		jsContent = JSON.parse(data);
+		return jsContent;
+	});*/
+	var objects;
+	if (type === "dressed") {
+		objects = globalElements;
+	} else if (type === "recomended") {
+		objects = recomended;
+	} else if (type === "recent") {
+		objects = recent;
+	}
+	return objects;
+}
+
+function postContent(contentObj, type) {
+	/*var url;
+	if (type === "recomended") {
+		url = "";
+	} else if (type === "recent") {
+		url = "";
+	} else {
+		url = "";
+	};
+	$.ajax({
+	    type: "POST",
+	    url: url,
+	    data: JSON.stringify(contentObj),
+	    contentType: "application/json; charset=utf-8",
+	    dataType: "json",
+	    success: function(data){
+	    	console.log("postContent: ");
+	    	console.log(contentObj);
+	    },
+	    failure: function(errMsg) {
+	        console.error("postContent");
+	    }
+	});*/
+	if (type === "recomended") {
+		recomended = contentObj;
+	} else if (type === "recent") {
+		dressed = contentObj;
+	} else {
+		globalElements = contentObj;
+	}
+}
+
+function dressIt(elem) {
+	var dressed = $(elem).clone();
+	var type = $(dressed).attr("type");
+	var sameItem = $("#canvas").find("[type='" + type + "']");
+	$(dressed).addClass("dressed");
+	if (sameItem.length > 0) {
+		undressIt(sameItem);
+	}
+	$("#canvas").append($(dressed));
+	//Gambiarra para concertar um erro esquisito
+	$("#canvas").find(".label").remove();
+	setDressedPieces($("#canvas"));
+
+	getObjByAttr("id", $(elem).attr("id")).isDressed = true;
+	copyProduct(getObjByAttr("id", $(elem).attr("id")), $("#content_recent"));
+}
 
 function vAlign(elements) {
 	$(elements).each(function(i, e) {
@@ -45,11 +129,96 @@ function createElementContainer(container, callback) {
 	}
 }
 
-function createElements(objectElem) {
+function copyProduct(product, destination) {
+	var type = $(destination).attr("id").substr(8);
+	var content = [];
+	var listId = getContent("recent");
+	if (type === "recent") {
+		for (var i = 0; i < listId.length; i++) {
+			content.push(getObjByAttr("id", listId[i]["id"]));
+		}
+	} else if (type === "fav") {
+		content = getContent("fav");
+	}
+	if (content === undefined) {
+		content = [];
+	};
+
+	var duplicate = false;
+	for (var i = 0; i < content.length; i++) {
+		if (content[i].id === $(product).attr("id")) {
+			duplicate = true;
+		}
+	}
+	/*if (!duplicate) {
+		content.push(product);	
+		postContent(content, type);
+		postContent(.push(product.attr("id")), type);
+		createBtnsCat(destination, content);
+	}*/
+	content.push(product);
+	postContent(content, type);
+	createBtnsCat(destination, content);
+}
+
+function createBtnsCat(parent, list) {
+	if ($(parent).children($(".catElements")).length > 0) {
+		$(parent).children($(".catElements")).remove();
+	}
 	var ulElements = $("<ul class='catElements fLeft'></ul>");
+	var category = $(parent).attr("id").substr(8);
+	$(list).each(function(i, e) {
+		var thisElem = $("<li class='bShadowLight rCSmall border fLeft' ondrag='dragIt(event, this)' onclick='selectProd(this)' draggable=true></li>");
+
+		var thisImg = $("<img id='" + e.id + "' class='draggable " + category + "' ondrag='dragIt(event, this)' onclick='selectProd(this)' src='images/" + e.id + ".png'>");
+		    thisImg.attr("title", e.title);
+		    thisImg.attr("type", e.type);
+
+		if (category === "roupas") {
+			var thisLabel = $("<div class='label fullWidth'></div>");
+		    thisLabel.append("<h3>" + e.title + "</h3>");
+			if (e.marca !== "" && e.marca !== undefined && e.marca !== null) {
+				thisLabel.append("<h4>" + e.marca + "</h4>");
+			} else {
+				thisLabel.append("<h4>Marca não definida</h4>");
+			}
+			thisLabel.attr('unselectable', 'on')
+                     .css('user-select', 'none')
+                     .on('selectstart', false);
+		}
+		$(parent).append(ulElements);
+		ulElements.append(thisElem);
+		thisElem.append(thisImg, thisLabel);
+
+		if (e.position !== "" && e.position !== undefined && e.position !== null) {
+			thisImg.css("top", e.position.top)
+			       .css("left", e.position.left);
+		}
+		if (e.layer !== "" && e.layer !== undefined && e.layer !== null) {
+			thisImg.css("z-index", e.layer);
+		}
+		if (e.isDressed) {
+			thisElem.addClass("toSelect");
+		}
+	});
+	$(parent).append(ulElements);
+	$(ulElements).children("li:nth-child(3n)").css("margin-right", 0);
+	if ($(ulElements).children().length >= 12) {
+		scrollPages($(parent), list);
+	}
+
+	setLabels($("#content_roupas .catElements li"));
+	centerImg($(".catElements li"));
+}
+
+function createElements(objectElem) {
 	var category = $(objectElem).attr("id").substr(8);
 
-	$(globalElements[category]).each(function(i, e) {
+	//$(objectElem).append(ulElements);
+
+	createBtnsCat($(objectElem), $(globalElements[category]));
+	
+	/*$(globalElements[category]).each(function(i, e) {
 		var thisElem = $("<li class='bShadowLight rCSmall border fLeft' ondrag='dragIt(event, this)' onclick='selectProd(this)' draggable=true></li>");
 
 		var thisImg = $("<img id='" + e.id + "' class='draggable " + category + "' ondrag='dragIt(event, this)' onclick='selectProd(this)' src='images/" + e.id + ".png'>");
@@ -88,7 +257,7 @@ function createElements(objectElem) {
 	}
 
 	setLabels($("#content_roupas .catElements li"));
-	centerImg($(".catElements li"));
+	centerImg($(".catElements li"));*/
 }
 
 function setLabels(elemList) {
@@ -148,10 +317,37 @@ function fixColors(elemList) {
 }
 
 function changeTab(tab) {
+	$(".containerContent").show();
+	$(".pages").hide();
+	$(".menu li.selected").removeClass("selected");
+	$(".pages li.active").removeClass("selected");
+	$(".pages li").eq($(tab).index()).removeClass("active");
 	$(".containerMenu li.active").removeClass("active");
 	$(".containerContent li.active").removeClass("active");
 	$(tab).addClass("active");
 	$(".containerContent>li").eq($(tab).index()).addClass("active");
+}
+
+function changePage(btnClicked) {
+	$(".containerContent").hide();
+	$(".pages").show();
+	$(".menu li.selected").removeClass("selected");
+	$(".pages li.selected").removeClass("selected");
+	$(".containerMenu li.active").removeClass("active");
+	$(".containerContent li.active").removeClass("active");
+	$(".pages>li").eq($(btnClicked).index()).addClass("selected");
+	$(btnClicked).addClass("selected");
+}
+
+function createPagesContent() {
+	var list = $("<ul class='category pages fullWidth bShadowLight rCBottom border'></ul>");
+	$(".menu li").each(function(i, e) {
+		var listItem = $("<li id='" + $(e).attr("auxid") + "'></li>");
+		var catElements = $("<ul class='catElements fLeft'></ul>");
+		listItem.append(catElements);
+		list.append(listItem);
+	});
+	$("#elements").append(list);
 }
 
 function scrollPages(parent, list) {
@@ -162,6 +358,7 @@ function scrollPages(parent, list) {
 	}
 	controller += "<span onclick='scrollThis(this)' class='next'>></span>";
 	$(parent).append($(controller));
+	console.log($(".controller .page").eq(0));
 	$(".controller .page").eq(0).click();
 }
 
@@ -209,41 +406,37 @@ function unselectIt(elem) {
 		   .removeAttr("elemId");
 }
 
-function dressIt(elem) {
-	var dressed = $(elem).clone();
-	var type = $(dressed).attr("type");
-	var sameItem = $("#canvas").find("[type='" + type + "']");
-	$(dressed).addClass("dressed");
-	if (sameItem.length > 0) {
-		undressIt(sameItem);
-	}
-	$("#canvas").append($(dressed));
-	//Gambiarra para concertar um erro esquisito
-	$("#canvas").find(".label").remove();
-	console.log("dressIt");
-	setDressedPieces($("#canvas"));
-}
-
 function setDressedPieces(parent) {
 	var dressed = [];
 	$(parent).find(".dressed").each(function(i, e) {
 		var prodId = $(e).attr("id");
-		for (var i = 0; i < Object.keys(globalElements).length; i++) {
-			var category = Object.keys(globalElements)[i];
-			$.grep(globalElements[category], function(j){
-				if (j.id === prodId) {
-					dressed.push(j);
-				}
-			});
+		if (getObjByAttr("id", prodId).length > 0) {
+			dressed.push(getObjByAttr("id", prodId));
 		}
 	});
-	console.log(dressed);
+	//postContent(dressed, "dressed");
+}
+
+function getObjByAttr(attr, value) {
+	var theObject;
+	for (var i = 0; i < Object.keys(globalElements).length; i++) {
+		var category = Object.keys(globalElements)[i];
+		$.grep(globalElements[category], function(j){
+			if (j[attr] === value) {
+				theObject = j;
+			}
+		});
+	}
+	return theObject;
 }
 
 function undressIt(elem) {
 	unselectIt($("[elemId='" + $(elem).attr("id") + "']"));
 	$(elem).remove();
 	setDressedPieces($("#canvas"));
+	console.log($(elem));
+	getObjByAttr("id", $(elem).attr("id")).isDressed = false;
+	postContent(globalElements);
 }
 
 function selectProd(elem) {
@@ -261,9 +454,27 @@ function selectProd(elem) {
 }
 
 function initialize() {
-	$("[type='modelo']").eq(0).parent().click();
-	$("[type='background']").eq(0).parent().click();
-	$("[type='panties']").eq(0).parent().click();
-	$("[type='soutien']").eq(0).parent().click();
-	$("[type='shadow']").eq(0).parent().click();
+		/*$("[type='modelo']").eq(0).parent().click();
+		$("[type='background']").eq(0).parent().click();
+		$("[type='panties']").eq(0).parent().click();
+		$("[type='soutien']").eq(0).parent().click();
+		$("[type='shadow']").eq(0).parent().click();	
+	*/
+	var recent = [];
+	for (var i = 0; i < getContent("recent").length; i++) {
+		recent.push(getObjByAttr("id", getContent("recent")[i]["id"]));
+	}
+
+	var recomended = [];
+	for (var i = 0; i < getContent("recomended").length; i++) {
+		recomended.push(getObjByAttr("id", getContent("recomended")[i]["id"]));
+	}
+
+	$(".toSelect").each(function(i, e) {
+		$(e).click();
+		$(e).removeClass("toSelect");
+	});
+
+	createBtnsCat($("#content_recent"), recent);
+	createBtnsCat($("#content_recomended"), recomended);
 }
